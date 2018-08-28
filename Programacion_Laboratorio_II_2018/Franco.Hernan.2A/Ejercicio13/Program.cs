ef120221ee73994ba2655ea3c49e80ee4649ee44
Elpiola <porve.cabj@gmail.com>
2018-08-28 08:39:11 -0300

    ejercicio 13

--- Programacion_Laboratorio_II_2018/Franco.Hernan.2A/Ejercicio13/Program.cs
+++ Programacion_Laboratorio_II_2018/Franco.Hernan.2A/Ejercicio13/Program.cs
@@ -13,14 +13,35 @@ namespace Ejercicio13
         {
             Console.Title = "Ejercicio 13 Conversor";
             Console.ForegroundColor = ConsoleColor.Green;
-            double numero;
-            string respuesta;
+            double numeroDecimal;
+            int opcion;
+            string numeroBinario;
 
-            Console.WriteLine("Ingrese un numero: ");
-            numero = double.Parse(Console.ReadLine());
+            Console.Write("\n\t¿Que desea convertir? \n\t Decimal a Binario = [1]\n\t Binario a Decimal = [2]\n\tRespuesta: ");
+            opcion = int.Parse(Console.ReadLine());
 
-            respuesta = Conversor.DecimalBinario(numero);
-            Console.WriteLine("El numero {0} se convirtio en binario: {1}", numero, respuesta);
+            if(opcion == 1)
+            {
+                Console.WriteLine("\n\t\t\tDECIMAL A BINARIO\n");
+                Console.Write("Ingrese un numero Decimal: ");
+                numeroDecimal = double.Parse(Console.ReadLine());
+                numeroBinario = Conversor.DecimalBinario(numeroDecimal);
+                Console.WriteLine("El numero {0} se convirtio en binario: {1}", numeroDecimal, numeroBinario);
+            }
+            else if (opcion == 2)
+            {
+                Console.WriteLine("\n\t\t\tBINARIO A DECIMAL\n");
+                Console.Write("Ingrese un numero Binario: ");
+                numeroBinario = Console.ReadLine();
+                numeroDecimal = Conversor.BinarioDecimal(numeroBinario);
+                Console.WriteLine("El numero binario {0} se convirtio en decimal: {1}", numeroBinario,numeroDecimal);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\t\t\tLA OPCION ELEGIDA NO ES CORRECTA!");
+            }
+
             Console.ReadLine();
         }
     }
