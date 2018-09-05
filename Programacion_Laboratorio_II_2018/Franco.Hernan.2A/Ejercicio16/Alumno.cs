4c74da2c82ed2f1fa7974bf42090e4e7efb011c1
elpiola <porve.cabj@gmail.com>
2018-09-04 23:43:22 -0300

    Ejercicio 16 y 17

---
+++ Programacion_Laboratorio_II_2018/Franco.Hernan.2A/Ejercicio16/Alumno.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    class Alumno
+    {
+        #region atributos
+        private byte _Nota1;
+        private byte _Nota2;
+        private float _NotaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+        #endregion
+
+        #region metodos
+        
+        public Alumno(string n, string a, int l)
+        {
+            this.nombre = n;
+            this.apellido = a;
+            this.legajo = l;
+        }
+
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this._Nota1 = notaUno;
+            this._Nota2 = notaDos;
+            CalcularFinal();
+        }
+
+        public void CalcularFinal()
+        {
+            if (this._Nota1 >= 4 && this._Nota2 >= 4)
+            {
+                this._NotaFinal = (float)(this._Nota1 + this._Nota2) / 2;
+            }
+            else
+            {
+                this._NotaFinal = -1;
+            }
+        }
+
+        public void Mostrar()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            string mostrar = "";
+            mostrar += "\nNombre: ";
+            mostrar += this.nombre;
+            mostrar += "\nApellido: ";
+            mostrar += this.apellido;
+            mostrar += "\nLegajo: ";
+            mostrar += this.legajo;
+            mostrar += "\nPrimer Nota: ";
+            mostrar += this._Nota1;
+            mostrar += "\nSegunda Nota: ";
+            mostrar += this._Nota2;
+            mostrar += "\nNota Final: ";
+            mostrar += this._NotaFinal;
+            mostrar += "\n";
+            Console.Write(mostrar);
+        }
+        #endregion
+    }
+}
---
+++ Programacion_Laboratorio_II_2018/Franco.Hernan.2A/Ejercicio17/Biografo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17
+{
+    class Biografo
+    {
+        #region atributos
+        public static short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+        #endregion
+
+        #region metodos
+
+        public Biografo(ConsoleColor c, short t)
+        {
+            this.color = c;
+            this.SetTinta(t);
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            if (tinta > 0 && tinta <= 100)
+            {
+                this.tinta = tinta;
+            }
+        }
+
+        public void Recargar()
+        {
+            this.tinta = cantidadTintaMaxima;
+        }
+
+        public bool Pintar(int gasto,out string dibujo)
+        {
+            bool retorno = false;
+            dibujo = "";
+            int resultado = this.GetTinta() - gasto;
+            if(resultado > 0)
+            {
+                for(int i = 0; i < gasto; i++)
+                {
+                    dibujo += "*";
+                }
+                Console.BackgroundColor = this.GetColor();
+                Console.Write("\nNivel gastado de tinta: {0}\n", dibujo);
+                Console.Write("\nCantidad de tinta que sobro: {0}\n", resultado);
+                retorno = true;
+            }
+            return retorno;
+        }
+        #endregion
+    }
+}
