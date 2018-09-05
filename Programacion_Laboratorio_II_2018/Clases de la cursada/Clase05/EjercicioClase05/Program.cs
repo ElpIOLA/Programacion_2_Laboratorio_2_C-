4cd6cc25c8da0920f7e37630fbcdc96ae4aa77ee
ElpIOLA <porve.cabj@gmail.com>
2018-09-05 10:59:55 -0300

    Ejercicio clase 05

--- Programacion_Laboratorio_II_2018/Clases de la cursada/Clase05/EjercicioClase05/Program.cs
+++ Programacion_Laboratorio_II_2018/Clases de la cursada/Clase05/EjercicioClase05/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +10,59 @@ namespace EjercicioClase05
     {
         static void Main(string[] args)
         {
-            Tinta pluma1 = new Tinta();
-            Tinta pluma2 = new Tinta();
-            Console.WriteLine(Tinta.Mostrar(pluma1));
-            Console.WriteLine(pluma1 == pluma2);
-            Tinta pluma3 = new Tinta(ConsoleColor.Red);
-            Console.WriteLine(pluma1 == pluma3);
-            Console.WriteLine(pluma1 != pluma3);
-            Console.ReadLine();
+        /*Tinta pluma1 = new Tinta();
+        Tinta pluma2 = new Tinta();
+
+        Console.WriteLine(Tinta.Mostrar(pluma1));
+        Console.WriteLine(pluma1 == pluma2);
+
+        Tinta pluma3 = new Tinta(ConsoleColor.Red);
+        Console.WriteLine(pluma1 == pluma3);
+        Console.WriteLine(pluma1 != pluma3);*/
+
+        Tinta pluma0 = new Tinta(ConsoleColor.DarkCyan);
+        Pluma lapicera = new Pluma("firuleti",50, pluma0);
+        Console.Write(lapicera);
+
+        if(lapicera == pluma0)
+        {
+            Console.Write("\nSon iguales!\n");
+        }
+        else
+        {
+            Console.Write("\n\tNo son iguales!\n");
+        }
+        Console.ReadLine();
+
+        Tinta pluma3 = new Tinta(ConsoleColor.DarkMagenta);
+        Pluma lapicera3 = new Pluma("grido", 70, pluma3);
+        Console.Write(lapicera3);
+
+        if (lapicera3 != pluma3)
+        {
+            Console.Write("\nSon iguales!\n");
+        }
+        else
+        {
+            Console.Write("\nNo son iguales!\n");
+        }
+        Console.ReadLine();
+
+            Tinta pluma1 = new Tinta(ConsoleColor.DarkGreen);
+        Pluma lapicera_1 = new Pluma("kalawa", 25, pluma1);
+        Console.Write(lapicera_1);
+        lapicera_1 = lapicera_1 + pluma1;
+        Console.Write("\n\tSUMA\n");
+        Console.Write(lapicera_1);
+        Console.ReadLine();
+
+        Tinta pluma2 = new Tinta(ConsoleColor.DarkYellow);
+        Pluma lapicera_2 = new Pluma("ggg", 46, pluma2);
+        Console.Write(lapicera_2);
+        lapicera_2 = lapicera_2 - pluma2;
+        Console.Write("\n\tRESTA\n");
+        Console.Write(lapicera_2);
+        Console.ReadLine();
         }
     }
 }
--- Programacion_Laboratorio_II_2018/Clases de la cursada/Clase05/EjercicioClase05/Tinta.cs
+++ Programacion_Laboratorio_II_2018/Clases de la cursada/Clase05/EjercicioClase05/Tinta.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,12 +35,14 @@ namespace EjercicioClase05
         #region Metodos
         public static string Mostrar(Tinta objeto)
         {
-            return objeto.Mostrar();
-
+            if (!Object.Equals(objeto, null))
+                return objeto.Mostrar();
+            return "Tinta Null";
         }
 
         private string Mostrar()
         {
+            Console.BackgroundColor = this._color;
             return "Color: " + this._color.ToString() + "\nTinta: " + this._tipo.ToString();
         }
 
@@ -61,6 +63,123 @@ namespace EjercicioClase05
             return !(obj1 == obj2);
         }
 
+        #endregion
+    }
+
+    class Pluma
+    {
+    #region Atributos
+    private string _marca;
+    private int _cantidad;
+    private Tinta _tinta;
+    #endregion
+
+    #region Constructores
+    public Pluma()
+    {
+      this._marca = "Sin Marca";
+      this._cantidad = 0;
+      this._tinta = null;
+    }
+
+    public Pluma(string m) : this()
+    {
+      this._marca = m;
+    }
+
+    public Pluma(int c) : this()
+    {
+      this._cantidad = c;
+    }
+
+    public Pluma(Tinta t) : this()
+    {
+      this._tinta = t;
+    }
+
+    public Pluma(string m, int c) : this(m)
+    {
+      this._cantidad = c;
+    }
+
+    public Pluma(string m, Tinta t) : this(m)
+    {
+      this._tinta = t;
+    }
+
+    public Pluma(int c, Tinta t) : this(c)
+    {
+      this._tinta = t;
+    }
+
+    public Pluma(string m, int c, Tinta t) : this(m, c)
+    {
+      this._tinta = t;
+    }
+    #endregion
+
+        #region Metodos
+        private string Mostrar()
+        {
+          string retorno = "";
+          retorno += "\nMarca: ";
+          retorno += this._marca;
+          retorno += "\nCantidad: ";
+          retorno += this._cantidad;
+          retorno += "\nTinta: ";
+          retorno += Tinta.Mostrar(this._tinta);
+          retorno += "\n";
+          return retorno;
+        }
+
+        public static implicit operator string(Pluma lapicera)
+        {
+          return lapicera.Mostrar();
+        }
+
+        public static bool operator ==(Pluma lapicera, Tinta tinta)
+        {
+            if(!object.Equals(tinta, null))
+                return lapicera._tinta == tinta;
+            return false;
+        }
+
+        public static bool operator !=(Pluma lapicera, Tinta tinta)
+        {
+            if(!object.Equals(tinta, null))
+                return !(lapicera == tinta);
+            return false;
+        }
+
+        public static Pluma operator +(Pluma lapicera, Tinta tinta)
+        {
+            int cantidad = 0;
+            if((lapicera == tinta && lapicera._cantidad < 100) || !object.Equals(tinta, null))
+            {
+                cantidad = lapicera._cantidad;
+                cantidad += 10;
+                if(cantidad < 100)
+                {
+                    lapicera._cantidad += 10;
+                }
+            }
+            return lapicera;
+        }
+
+        public static Pluma operator -(Pluma lapicera, Tinta tinta)
+        {
+            int cantidad = 0;
+            if ((lapicera == tinta && lapicera._cantidad  > 10 && lapicera._cantidad < 100) || !object.Equals(tinta,null))
+            {
+                cantidad = lapicera._cantidad;
+                cantidad -= 10;
+                if (cantidad > 0)
+                {
+                    lapicera._cantidad -= 10;
+                }
+            }
+            return lapicera;
+        }
         #endregion
     }
 }
